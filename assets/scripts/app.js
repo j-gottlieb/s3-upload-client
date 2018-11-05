030885a2631d03ea37070b63f3e8ff5f510d62c4
j-gottlieb <gessejottlieb@gmail.com>
2018-11-05 16:14:06 -0500

    Created upload form with success message div

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,26 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const config = require('./config.js')
 
 $(() => {
-  // your JS code goes here
+  $('#upload-form').on('submit', function (event) {
+    event.preventDefault()
+    const data = new FormData(event.target)
+    $.ajax({
+      method: 'POST',
+      url: config.apiUrl + '/uploads',
+      processData: false,  // Important!
+      contentType: false,
+      data: data
+    })
+      .then((response) => {
+        console.log(response)
+        $('#new-image').attr('src', response.upload.url)
+      })
+      .then($('#upload-message').html('Upload Success!'))
+      .catch((response) => {
+        console.log(response)
+      })
+  })
 })
